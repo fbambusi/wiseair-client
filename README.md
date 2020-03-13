8fc09a502d234736f693fd734d03bbad2c018f1c
fbambusi <fbambusi@github.com>
2020-03-13 09:43:56 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
-# wiseair-client
+# Welcome to WiseairClient!
 
-A python wrapper for Wiseair API
+WiseairClient is a wrapper to access Air Quality data provided by  [Wiseair](https://www.wiseair.it).
+
+
+## Data
+
+Our data are gathered real-time by a distributed and capillar network of sensors. 
+You can find the openapi specification of the API and the data [here](https://wiseair-development-utils.s3.eu-central-1.amazonaws.com/api-specifications/production/v1/openapi.yml).
+
+## Structure
+The main files in this repository are:
+
+- WiseairClient.py: This python module is the bulk of the software. 
+- 01-hello-world.ipynb: A "hello world" Jupyter Notebook that shows the functionlities of WiseairClient. 
+- 02-hello-world.py: A  "hello world" Python script that shows the functionlities of WiseairClient. 
+
+## Setup
+
+To use WiseairClient:
+
+ 1. Download Wiseair App: [here](https://play.google.com/store/apps/details?id=com.wiseair) the Google Play Store link, [here](https://apps.apple.com/it/app/wiseair/id1489703565?l=en&fbclid=IwAR3cegztyvSOKsc2cMU7msV5Lirz5XxA7ZKEV_uAmhrDd39CON5wHU7UmI4) the Apple App Store link
+ 2. Open the App and register an account
+ 3. Rename "clientCredentialsMock.csv" in "clientCredentials.csv" and fill it with your data: 
+ "client_id","client_secret","user_email","user_password"
+ YY,"XXX","YOUR_EMAIL","YOUR_PASSWORD"
+ 4. You should have received a client id and a client secret with the invitation to this project: if this is not the case, ask [our IT](mailto:fulvio.bambusi@wiseair.it) .
+ 5. Launch the "hello world" script: it will download the data gathered in the last week by one sensor, and store them in sampleData.csv
+
+```
+`python3 02-hello-world.py`
+```
+  ## Troubleshooting & Feedback
+
+If you meet any problem, please [open an issue](https://gitlab.com/wiseair-group/wiseair-client/-/issues/new). If you have cool ideas for new functionalities or you simply want to share with us your opinion, contact us at [contact@wiseair.it](mailto:contact@wiseair.it).
+
+
+
+ 
+
+
---
+++ WiseairClient.py
@@ -0,0 +1,184 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[82]:
+
+
+import csv
+import requests
+import json
+import pandas as pd
+import json
+import datetime
+from datetime import date
+from datetime import timedelta
+
+
+class WiseairClient:
+    
+    def __putJson(self,url,data):
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        headers["Authorization"]="Bearer {}".format(self.__userToken)
+        r = requests.put(url, data=json.dumps(data), headers=headers)
+        return r
+    def __postJson(self,url,data):
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        headers["Authorization"]="Bearer {}".format(self.__userToken)
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        return json.loads(r.content.decode("utf-8"))
+    def __getJson(self,url,data):
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        headers["Authorization"]="Bearer {}".format(self.__userToken)
+        r = requests.get(url, data=json.dumps(data), headers=headers)
+        return json.loads(r.content.decode("utf-8"))
+    
+    
+    def __getClientToken(self,clientId,clientSecret):
+        url=self.__baseUrl+"/oauth/token"
+        data={"client_id":clientId,"client_secret":clientSecret,"grant_type":"client_credentials"}
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        return json.loads(r.text)["access_token"]
+
+    def __getPersonalToken(self,userEmail,userPassword,clientToken):
+        url=self.__baseUrl+"/api/auth/login"
+        data={"email":userEmail,"password":userPassword,"grant_type":"client_credentials"}
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        headers["Authorization"]="Bearer {}".format(clientToken)
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        return json.loads(r.text)["access_token"]
+
+    def __init__(self,pathToClientCredentials,baseUrl="https://api.wiseair.it"):
+        with open(pathToClientCredentials,"r") as csvFile:
+            dic=csv.DictReader(csvFile)
+            for row in dic:
+                self.__clientId=row["client_id"]
+                self.__clientSecret=row["client_secret"]
+                self.__userEmail=row["user_email"]
+                self.__userPassword=row["user_password"]
+        self.__baseUrl=baseUrl
+        self.__oldUrl="https://www.wiseair.it/backend-test/public"
+        self.__clientToken=self.__getClientToken(self.__clientId,self.__clientSecret)
+        self.__userToken=self.__getPersonalToken(self.__userEmail,self.__userPassword,self.__clientToken)
+
+    def __getIntervalBetweenLastTenMeasures(self,potId,fromDate,toDate):
+        results=self.getDataOfPotByInterval(potId,fromDate,toDate)
+        df=pd.DataFrame(results)
+        df['ts'] =pd.DatetimeIndex(df.created_at).asi8//(10**9)
+        return df["ts"].diff()[1:]
+
+    def __isHavingRegularPace(self,timeIntervalBetweenLastMeasuresInSecond):
+        variance=np.std(timeIntervalBetweenLastMeasuresInSecond)
+        mean=np.mean(timeIntervalBetweenLastMeasuresInSecond)
+        rep=variance/mean
+        
+        #extremely regular: std/mean close to zero: 0.003 for correct sensor
+        # 1.22 for exponential sleep
+        if rep>0.2 :
+            return False
+        return True
+        
+    def lastTenMeasuresOfSensorAreEquallyPaced(self,sensorId):
+        today = date.today()
+        differences=self.__getIntervalBetweenLastTenMeasures(sensorId,str(today-timedelta(days=1)),str(today+timedelta(days=1)))
+        return self.__isHavingRegularPace(differences[-10:])
+
+    def    getDataOfPotByPage(self,pot_id,pageNr):
+        url=self.__baseUrl+"http://www.wiseair.it/backend-test/public/measures/by-pot/{}?page={}".format(pot_id,pageNr)
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        headers["Authorization"]="Bearer {}".format(self.__userToken)
+        r = requests.get(url, headers=headers)
+        data=json.loads(r.text)["data"]
+        return data
+    
+    def getDataOfPotByInterval(self,pot_id,fromDate,toDate):
+        url=self.__baseUrl+"/api/measures-by-time-interval"
+        data={"until_date":toDate,"from_date":fromDate,"pot_id":pot_id}
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        headers["Authorization"]="Bearer {}".format(self.__userToken)
+        r = requests.get(url, data=json.dumps(data), headers=headers)
+        return json.loads(r.text)
+    
+    
+    def createPot(self,longitude,latitude):
+        data={"latitude":latitude,"longitude":longitude,"pm2p5":-1,"pm10":-1}
+        url=self.__baseUrl+"/api/create-pot"
+        print(url)
+        print(data)
+        response=self.__postJson(url,data)
+        return response
+    
+    def registerPot(self,activationCode,country,city,streetName,houseNumber,postalCode):
+        data={
+            "activation_token":activationCode,
+            "streetname":streetName,
+            "housenumber":houseNumber,
+            "city":city,
+            "postalcode":postalCode
+        }
+        url=self.__baseUrl+"/api/activate-pot"
+        response=self.__putJson(url,data)
+        print(response)
+        print(response.content)
+
+    def createMeasure(self,chipId="",pm1=-1,pm2p5=-1,pm4=-1,pm10=-1,humidity=-1,temperature=-1,voltage=-1):
+        data={
+            "chip_id": chipId,
+            "pm2p5": pm2p5,
+            "pm10": pm10, 
+            "humidity": humidity,
+            "temperature": temperature,
+            "voltage": voltage, 
+            "pm1SPS": pm1,
+            "pm2p5SPS":pm2p5,
+            "pm10SPS": pm10,
+            "pm4SPS": pm4}
+        url=self.__baseUrl+"/measures/createV2"
+        response=self.__postJson(url,data)
+        return response
+    
+    def getAllLocations(self):
+        url=self.__baseUrl+"/api/get-all-locations"
+        response=self.__getJson(url,{})
+        return response
+    
+    def getStateOfPots(self,page=1):
+        data={
+            "page": page,
+            }
+        url=self.__baseUrl+"/api/get-state-of-pots"
+        response=self.__getJson(url,data)
+        return response
+
+    def getLiveAirQuality(self,latitude="45.458453",longitude="9.1782493",page=0):
+        data={
+            "longitude":longitude,
+            "latitude":latitude,
+            "tolerance":"40",
+            "untilDate":datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "items":100,
+            "page":page
+        }
+        url=self.__baseUrl+"/api/live-air-quality"
+        response=self.__getJson(url,data)
+        return response
+    
+class WiseairUtils:
+    def __init__(self):
+        pass
+    def getPandasDataFrameFromDataOfSingleSensor(self,pollutionData):
+        data=pd.DataFrame(pollutionData)
+        data["created_at"]=pd.to_datetime(data["created_at"])
+        data.index=data["created_at"]
+        data.drop("created_at",axis=1,inplace=True)
+        return data
+
+
+
