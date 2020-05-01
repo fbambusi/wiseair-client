09066b4d1b3bb2370b663818d117ff7da93819a5
fbambusi <fbambusi@github.com>
2020-05-01 10:01:33 +0200

    Remove json requirement

--- setup.py
+++ setup.py
@@ -13,9 +13,7 @@ setup(
   install_requires=[            # I get to this in a second
         'pandas',
         'numpy',
-        "requests",
-        "json",
-        "json"
+        "requests"
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
